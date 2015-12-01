d888a61deaf4fe3c61650220e60fe9cceddabe4d
Fangming Chai <fachai@cisco.com>
2015-11-30 17:45:19 -0800

    remove unused method in TXCache.java

--- spi/src/main/java/org/opendaylight/distributed/tx/spi/TxCache.java
+++ spi/src/main/java/org/opendaylight/distributed/tx/spi/TxCache.java
@@ -1,17 +1,10 @@
 package org.opendaylight.distributed.tx.spi;
 
-import com.google.common.util.concurrent.CheckedFuture;
-import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 /**
  * APIs for a caching transaction. Providing cached data to Rollback execution.
  */
 public interface TxCache extends Iterable<CachedData> {
-
-    public CheckedFuture<Void, ReadFailedException> asyncDelete(final LogicalDatastoreType logicalDatastoreType,
-                                                                final InstanceIdentifier<?> instanceIdentifier);
 
 
 }
